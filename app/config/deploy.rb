34fec308a4c21bcfb46e1b5e1a9255ccc596a303
babi4 <babichevv@gmail.com>
2012-01-02 12:06:09 +0200

    deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -48,7 +48,7 @@ end
 # В случае с Rails 3 приложениями стоит заменять bundle exec unicorn_rails на bundle exec unicorn
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -9 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
   task :start do
     run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
